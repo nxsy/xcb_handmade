09ad38b95a3ff0557726285c74b34d8e50455f16
Some Name <some@mail>
2015-05-17 16:23:14 +1000

    suppress compiler warnings, include iaca header directory

--- build.sh
+++ build.sh
@@ -4,8 +4,9 @@ set -e
 set -u
 
 XCBLIBS="-lrt -lm -ldl -lxcb -lxcb-image -lxcb-icccm -lxcb-keysyms -lasound"
-CPPFLAGS="-DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1"
-WARNFLAGS="-Wall -Wno-unused-variable"
+CPPFLAGS="-I../iaca/include/ -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1"
+WARNFLAGS="-Wall -Wno-unused-variable -Wno-unused-but-set-variable\
+	 -Wno-write-strings -Wno-unused-function"
 GAMEWARNFLAGS="${WARNFLAGS} -Wno-sign-compare"
 DEBUG_FLAGS="-DDEBUG -g -DHANDMADE_SLOW=1 -DHHXCB_SLOW=1"
 OPT_FLAGS="-Ofast"

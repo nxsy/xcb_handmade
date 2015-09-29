89b7d3b89e231783fa476741e29f8efb6e4659cd
Some Name <some@mail>
2015-09-30 06:02:30 +1000

    after day 194, changed from c++11 compiler standard to gnu++11 for better variadic macro support

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@
 set -e
 set -u
 
+## was using c++11, but it didn't like variadic macros leaving a trailing
+## comma if the "__VA_ARGS__" was empty, so I switched to gnu++11
+CPPSTD="-std=gnu++11"
 XCBLIBS="-lrt -lm -ldl -lxcb -lxcb-image -lxcb-icccm -lxcb-keysyms -lxcb-randr -lasound"
 CPPFLAGS="-pthread -I../iaca/include/ -DHANDMADE_PROFILE=1 -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1"
 WARNFLAGS="-Wall -Wno-unused-variable -Wno-unused-but-set-variable\
@@ -47,11 +50,11 @@ then
 
 mkdir -p build/debug
 ## Asset file builder
-#g++ -std=c++0x -DTRANSLATION_UNIT_INDEX=0 ${WARNFLAGS} -o build/debug/multiplatform_test_asset_builder src/multiplatform_test_asset_builder.cpp ${CPPFLAGS} ${XCBLIBS} ${DEBUG_FLAGS} -lX11
+#g++ ${CPPSTD} -DTRANSLATION_UNIT_INDEX=0 ${WARNFLAGS} -o build/debug/multiplatform_test_asset_builder src/multiplatform_test_asset_builder.cpp ${CPPFLAGS} ${XCBLIBS} ${DEBUG_FLAGS} -lX11
 ## Optimized renderer build
-g++ -std=c++0x -DTRANSLATION_UNIT_INDEX=1 ${GAMEWARNFLAGS} -c src/handmade_optimized.cpp -o build/debug/handmade_optimized.o ${CPPFLAGS} -Ofast -fPIC
+g++ ${CPPSTD} -DTRANSLATION_UNIT_INDEX=1 ${GAMEWARNFLAGS} -c src/handmade_optimized.cpp -o build/debug/handmade_optimized.o ${CPPFLAGS} -Ofast -fPIC
 ## Shared library
-g++ -std=c++0x -DTRANSLATION_UNIT_INDEX=0 ${GAMEWARNFLAGS} -shared -Wl,-soname,libhandmade.so.1 -fPIC -o build/debug/libhandmade.so.new src/handmade.cpp build/debug/handmade_optimized.o ${CPPFLAGS} ${DEBUG_FLAGS}
+g++ ${CPPSTD} -DTRANSLATION_UNIT_INDEX=0 ${GAMEWARNFLAGS} -shared -Wl,-soname,libhandmade.so.1 -fPIC -o build/debug/libhandmade.so.new src/handmade.cpp build/debug/handmade_optimized.o ${CPPFLAGS} ${DEBUG_FLAGS}
 ## Overwrite the old shared library with the new one
 mv -f build/debug/libhandmade.so.new build/debug/libhandmade.so
 
@@ -60,7 +63,7 @@ mv -f build/debug/libhandmade.so.new build/debug/libhandmade.so
 #mv -f build/debug/libalternate.so.new build/debug/libalternate.so
 
 ## Platform code
-g++ -std=c++0x -DTRANSLATION_UNIT_INDEX=2 ${WARNFLAGS} -o build/debug/xcb_handmade src/xcb_handmade.cpp ${CPPFLAGS} ${XCBLIBS} ${DEBUG_FLAGS}
+g++ ${CPPSTD} -DTRANSLATION_UNIT_INDEX=2 ${WARNFLAGS} -o build/debug/xcb_handmade src/xcb_handmade.cpp ${CPPFLAGS} ${XCBLIBS} ${DEBUG_FLAGS}
 
 ## Alternate platform code
 #g++ -DGAME_CODE_FILENAME=libalternate.so -std=c++0x ${WARNFLAGS} -o build/debug/xcb_alternate src/xcb_handmade.cpp ${CPPFLAGS} ${XCBLIBS} ${DEBUG_FLAGS}
@@ -74,11 +77,11 @@ then
 
 mkdir -p build/opt
 ## Asset file builder
-#g++ -std=c++0x -DTRANSLATION_UNIT_INDEX=0 ${WARNFLAGS} -o build/opt/multiplatform_test_asset_builder src/multiplatform_test_asset_builder.cpp ${CPPFLAGS} ${XCBLIBS} -lX11
+#g++ ${CPPSTD} -DTRANSLATION_UNIT_INDEX=0 ${WARNFLAGS} -o build/opt/multiplatform_test_asset_builder src/multiplatform_test_asset_builder.cpp ${CPPFLAGS} ${XCBLIBS} -lX11
 ## Optimized renderer
-g++ -std=c++0x -DTRANSLATION_UNIT_INDEX=1 ${GAMEWARNFLAGS} -c src/handmade_optimized.cpp -o build/opt/handmade_optimized.o ${CPPFLAGS} -Ofast -fPIC
+g++ ${CPPSTD} -DTRANSLATION_UNIT_INDEX=1 ${GAMEWARNFLAGS} -c src/handmade_optimized.cpp -o build/opt/handmade_optimized.o ${CPPFLAGS} -Ofast -fPIC
 ## Shared library
-g++ -std=c++0x -DTRANSLATION_UNIT_INDEX=0 ${GAMEWARNFLAGS} -shared -Wl,-soname,libhandmade.so.1 -fPIC -o build/opt/libhandmade.so.new src/handmade.cpp build/debug/handmade_optimized.o ${CPPFLAGS} ${OPT_FLAGS}
+g++ ${CPPSTD} -DTRANSLATION_UNIT_INDEX=0 ${GAMEWARNFLAGS} -shared -Wl,-soname,libhandmade.so.1 -fPIC -o build/opt/libhandmade.so.new src/handmade.cpp build/debug/handmade_optimized.o ${CPPFLAGS} ${OPT_FLAGS}
 ## Overwrite the old shared library with the new one
 mv -f build/opt/libhandmade.so.new build/opt/libhandmade.so
 
@@ -87,7 +90,7 @@ mv -f build/opt/libhandmade.so.new build/opt/libhandmade.so
 #mv -f build/opt/libalternate.so.new build/opt/libalternate.so
 
 ## Platform code
-g++ -std=c++0x -DTRANSLATION_UNIT_INDEX=2 ${WARNFLAGS} -o build/opt/xcb_handmade src/xcb_handmade.cpp ${CPPFLAGS} ${XCBLIBS} ${OPT_FLAGS}
+g++ ${CPPSTD} -DTRANSLATION_UNIT_INDEX=2 ${WARNFLAGS} -o build/opt/xcb_handmade src/xcb_handmade.cpp ${CPPFLAGS} ${XCBLIBS} ${OPT_FLAGS}
 
 ## Alternate platform code
 #g++ -DGAME_CODE_FILENAME=libalternate.so -std=c++0x ${WARNFLAGS} -o build/opt/xcb_alternate src/xcb_handmade.cpp ${CPPFLAGS} ${XCBLIBS} ${OPT_FLAGS}
